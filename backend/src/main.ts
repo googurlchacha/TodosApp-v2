832c6a49204e8d4586c5690950759da319453d40
googurlchacha <googurlchacha@gmail.com>
2026-04-10 11:50:14 +0500

    updated the port of backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,13 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.setGlobalPrefix('api');
+//   app.enableCors();
+//   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+//   const port = process.env.PORT ?? 3000;
+//   await app.listen(port);
+// }
+
+// bootstrap();
+
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  const port = process.env.PORT ?? 3000;
-  await app.listen(port);
-}
 
+  const port = process.env.PORT || 8080;
+
+  await app.listen(port, '0.0.0.0');
+}
 bootstrap();
